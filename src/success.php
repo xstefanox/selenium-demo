82e0f73f29246eeba49a29c8dcdb49117c8f7c02
Stefano Varesi <stefano.varesi@gmail.com>
2016-05-10 22:03:20 +0200

    working tests

--- src/success.php
+++ src/success.php
@@ -5,11 +5,11 @@
     <div>Inserted values:</div>
     <div>
       <span>First Name:</span>
-      <span><?php echo $_SESSION['first-name'] ?></span>
+      <span id="first-name"><?php echo $_SESSION['first-name'] ?></span>
     </div>
     <div>
       <span>Last Name:</span>
-      <span><?php echo $_SESSION['last-name'] ?></span>
+      <span id="last-name"><?php echo $_SESSION['last-name'] ?></span>
     </div>
   </body>
 </html>
---
+++ test/FormTest.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace SeleniumDemo\Test;
+
+class FormTest extends \PHPUnit_Extensions_Selenium2TestCase
+{
+    protected function setUp()
+    {
+        $this->setBrowser('firefox');
+        $this->setBrowserUrl('http://localhost:8080');
+        $this->prepareSession();
+    }
+
+    public function testEmptyFormFields()
+    {
+        $this->url('/src/index.php');
+        sleep(1);
+
+        $this->byId('submit')->submit();
+
+        $this->assertNotEmpty('Leeroy', $this->byId('error-first-name')->text());
+        $this->assertNotEmpty('Jenkins', $this->byId('error-last-name')->text());
+        sleep(1);
+    }
+
+    public function testInvalidFormFields()
+    {
+        $this->url('/src/index.php');
+        sleep(1);
+
+        $this->byId('first-name')->value('@#^?!');
+        sleep(1);
+
+        $this->byId('last-name')->value('@#[]');
+        sleep(1);
+
+        $this->byId('submit')->submit();
+
+        $this->assertNotEmpty($this->byId('error-first-name')->text());
+        $this->assertNotEmpty($this->byId('error-last-name')->text());
+        sleep(1);
+    }
+
+    public function testSuccessfulForm()
+    {
+        $this->url('/src/index.php');
+        sleep(1);
+
+        $this->byId('first-name')->value('Leeroy');
+        sleep(1);
+
+        $this->byId('last-name')->value('Jenkins');
+        sleep(1);
+
+        $this->byId('submit')->submit();
+        sleep(1);
+
+        $this->assertEquals('Leeroy', $this->byId('first-name')->text());
+        $this->assertEquals('Jenkins', $this->byId('last-name')->text());
+
+        $this->assertEquals('Leeroy', $this->byCssSelector('#first-name')->text());
+        $this->assertEquals('Jenkins', $this->byCssSelector('#last-name')->text());
+
+        $this->assertEquals('Leeroy', $this->byXPath('//*[@id="first-name"]')->text());
+        $this->assertEquals('Jenkins', $this->byXPath('//*[@id="last-name"]')->text());
+
+        sleep(1);
+    }
+}
